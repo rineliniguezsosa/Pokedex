077404071e369aae46560766d299a7d3056b775b
rinel <rineliniguezsosa@gmail.com>
2023-05-06 16:43:49 -0600

    importando el store para proporcionar la lectura de los slice

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter } from 'react-router-dom'
 import { Approuter } from './router/Approuter'
 import { Provider } from 'react-redux'
+import { store } from './store/store'
 
 function App() {
   return (
