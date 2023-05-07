682ceb6ae14ecd4333cad3d9028471d37baa6f39
rinel <rineliniguezsosa@gmail.com>
2023-05-07 15:08:23 -0600

    cambiando el nombre de Divlogin por Contenedores ya agregando las propiedades display flex

---
+++ src/assets/components/Contenedores.jsx
@@ -0,0 +1,19 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+    height:640px; 
+    flex-wrap: wrap;
+    justify-content: center; 
+    align-items: center; 
+    border:1px red solid ; 
+    display: flex;
+    flex-direction: column;
+    padding: 10px;
+    width: 1325px;
+`;
+
+export const Divtitle = styled.div`
+    width: auto;
+    border:1px red solid;
+    height: auto;
+`
