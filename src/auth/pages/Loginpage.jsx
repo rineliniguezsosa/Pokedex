4cb4354d2843f2bad5f8794a04f245c92d667937
rinel <rineliniguezsosa@gmail.com>
2023-05-07 15:42:01 -0600

    importando Formcontainer

--- src/auth/pages/Loginpage.jsx
+++ src/auth/pages/Loginpage.jsx
@@ -1,5 +1,5 @@
 import { Formik,Form } from 'formik'
-import { Titlepokedex,Container,Divtitle } from '../../assets'
+import { Titlepokedex,Container,Divtitle,Formcontainer } from '../../assets'
 
 export const Loginpage = () => {
   return (
