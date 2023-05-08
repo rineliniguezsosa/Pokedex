9129d41bdaf6971b437e6f0d6070785f132a8a74
rinel <rineliniguezsosa@gmail.com>
2023-05-07 20:14:46 -0600

    agregando las propiedades css background-color:#47525E,border-radius:3px,border:2px solid,color:white,margin:0 1em,padding:0.25em 1em para el componente Submitbutton

--- src/assets/components/Buttons.jsx
+++ src/assets/components/Buttons.jsx
@@ -1,5 +1,10 @@
 import styled from 'styled-components'
 
 export const Submitbutton = styled.button`
-
+  background-color: #47525E;
+  border-radius: 3px;
+  border: 2px solid ;
+  color: white;
+  margin: 0 1em;
+  padding: 0.25em 1em;
 `
