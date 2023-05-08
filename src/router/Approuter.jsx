e1895de68d203ed5cc239d7e944a065cb1d2f58d
rinel <rineliniguezsosa@gmail.com>
2023-05-08 13:30:42 -0600

    imprimiendo en consola el valor de autenticado momentaneamente para observar el comportamiento

--- src/router/Approuter.jsx
+++ src/router/Approuter.jsx
@@ -4,6 +4,7 @@ import { Loginpage } from '../auth'
 
 export function Approuter() {
   const { autenticado } = useSelector(state => state.auth)
+  console.log(autenticado)
   return (
     <>
     <Routes>
