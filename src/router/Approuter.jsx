bd377129a147fc52dc60b213ec02825d7df0ba89
rinel <rineliniguezsosa@gmail.com>
2023-05-08 22:01:52 -0600

    importando Navigate de react-router-dom en Approuter.jsx

--- src/router/Approuter.jsx
+++ src/router/Approuter.jsx
@@ -1,4 +1,4 @@
-import { Routes,Route } from 'react-router-dom'
+import { Routes,Route,Navigate } from 'react-router-dom'
 import { useSelector } from 'react-redux'
 import { Pokemonroutes } from '../pokedex/routes/Pokemonroutes'
 import { Loginroutes } from '../auth/routes/Loginroutes'
