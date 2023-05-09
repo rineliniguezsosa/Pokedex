7e727ba4a28be43af02cd7223530704e205c78cf
rinel <rineliniguezsosa@gmail.com>
2023-05-08 22:07:00 -0600

    importando Navigate de react-router-dom en Pokemonroutes.jsx

--- src/pokedex/routes/Pokemonroutes.jsx
+++ src/pokedex/routes/Pokemonroutes.jsx
@@ -1,4 +1,4 @@
-import { Routes,Route } from 'react-router-dom'
+import { Routes,Route,Navigate } from 'react-router-dom'
 import { Pokedex } from '../pages/Pokedex'
 
 export const Pokemonroutes = () => {
