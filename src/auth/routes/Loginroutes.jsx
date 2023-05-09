b0696d808f842de9aef7fcc03dfd92e0be8314c7
rinel <rineliniguezsosa@gmail.com>
2023-05-08 22:21:12 -0600

    importando Loginpage en Loginroutes.jsx

--- src/auth/routes/Loginroutes.jsx
+++ src/auth/routes/Loginroutes.jsx
@@ -1,9 +1,10 @@
 import { Navigate, Route, Routes } from "react-router-dom"
+import { Loginpage } from "../pages/Loginpage"
 
 export const Loginroutes = () => {
   return (
     <Routes>
-      
+
     </Routes>
   )
 }
