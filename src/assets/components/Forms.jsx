2956977d97885bc46f9bebcc7d4a5db992241cea
rinel <rineliniguezsosa@gmail.com>
2023-05-10 00:59:11 -0600

    eliminando las propiedades css de el componente Form y creando el elemento Inputform con los atributos placeholder:Buscar pokemon,type:text y las propiedades width:250px,border-radius:3px #C6CDD5 y padding de 10px 10px

--- src/assets/components/Forms.jsx
+++ src/assets/components/Forms.jsx
@@ -1,8 +1,13 @@
 import styled from 'styled-components'
 
 export const Form = styled.form`
-
+    
 `
-export const Inputform = styled.input`
-
+export const Inputform = styled.input.attrs({
+    placeholder:'Buscar pokemon',
+    type:'text'
+})`
+    width:250px;
+    border-radius:3px #C6CDD5;
+    padding: 10px 10px;
 `
