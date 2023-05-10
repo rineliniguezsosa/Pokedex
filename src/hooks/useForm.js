5cedbc5513275c3265cfb8edaf601f319a094c05
rinel <rineliniguezsosa@gmail.com>
2023-05-10 01:54:14 -0600

    creamos onInputchange arrow function para actualizar el valor del input pokemon

--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -2,6 +2,10 @@ import { useState,useEffect } from "react"
 
 export const useForm = (initialState = {}) =>{
     const [form, setForm] = useState(initialState)
+
+    const onInputchange = () =>{
+        
+    }
     return {
 
     }
