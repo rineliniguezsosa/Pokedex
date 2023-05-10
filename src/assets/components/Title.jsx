e30c3b6ca89095ebd54c0b64ce06a4c3786f179d
rinel <rineliniguezsosa@gmail.com>
2023-05-09 22:02:14 -0600

    agregando la propiedad color:#47525E en Titlepokedex

--- src/assets/components/Title.jsx
+++ src/assets/components/Title.jsx
@@ -3,6 +3,7 @@ import styled from 'styled-components'
 export const Titlepokedex = styled.h1`
     font-weight: bolder;
     font-size: 30px;
+    color:#47525E;
 `
 export const Pokedextitle = styled(Titlepokedex)`
     font-size: 30px;
