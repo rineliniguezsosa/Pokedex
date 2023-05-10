1a027f6a918ac7137239ea824c88362465e589bf
rinel <rineliniguezsosa@gmail.com>
2023-05-09 21:36:30 -0600

    agregando el componente Pokedexcontainer este componente contendra el contenido del pokedex el titulo,el formulario y los elementos a desplegar

--- src/pokedex/pages/Pokemon.jsx
+++ src/pokedex/pages/Pokemon.jsx
@@ -4,7 +4,9 @@ import { Pokemoncontainer,Pokedextitle,Pokedexcontainer } from "../../assets"
 export const Pokemon = () => {
   return (
     <Pokemoncontainer>
-        <div style={{width:'800px',height:'500px',border:'1px yellow solid'}}></div>
+        <Pokedexcontainer>
+          
+        </Pokedexcontainer>
     </Pokemoncontainer>
   )
 }
