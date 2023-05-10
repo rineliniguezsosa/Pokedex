7dc74b0be3f2bd6bebb0a00ee2fa3a53463523ab
rinel <rineliniguezsosa@gmail.com>
2023-05-09 19:01:44 -0600

    cambiando el nombre de la importacion de Pokedexcontainer por Pokemoncontainer

--- src/pokedex/pages/Pokemon.jsx
+++ src/pokedex/pages/Pokemon.jsx
@@ -1,10 +1,10 @@
-import { Pokedexcontainer } from "../../assets"
+import { Pokemoncontainer } from "../../assets"
 
 
 export const Pokemon = () => {
   return (
     <Pokedexcontainer>
-        
+        <div style={{width:'800px',height:'500px',border:'1px yellow solid'}}></div>
     </Pokedexcontainer>
   )
 }
