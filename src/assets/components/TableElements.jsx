34fefe83f1d360455f753a09eac2af4a1e5b7041
rinel <rineliniguezsosa@gmail.com>
2023-05-11 14:55:24 -0600

    agregando las propiedades css  width:100%;height:100px;border:1px #E1E7EE solid;border-radius:2px;border-collapse:collapse; del componente Table en  TableElements.jsx

--- src/assets/components/TableElements.jsx
+++ src/assets/components/TableElements.jsx
@@ -1,5 +1,9 @@
 import styled from 'styled-components'
 
 export const Table = styled.table`
-
+    width:100%;
+    height:100px;
+    border:1px #E1E7EE solid;
+    border-radius:2px;
+    border-collapse:collapse;
 `
