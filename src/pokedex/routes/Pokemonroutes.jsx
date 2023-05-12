abd4e325939152675a928a96fcab574fdd16e5ee
rinel <rineliniguezsosa@gmail.com>
2023-05-12 14:41:26 -0600

    importando PokemonShiny en Pokemonroutes.jsx

--- src/pokedex/routes/Pokemonroutes.jsx
+++ src/pokedex/routes/Pokemonroutes.jsx
@@ -1,5 +1,6 @@
 import { Routes,Route,Navigate } from 'react-router-dom'
 import { Pokemon } from '../pages/Pokemon'
+import { PokemonShiny } from '../components/PokemonShiny'
 
 export const Pokemonroutes = () => {
   return (
