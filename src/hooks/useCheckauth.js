6c023b27ae80671d10ba12cf0150d37a33f410a2
rinel <rineliniguezsosa@gmail.com>
2023-05-12 13:16:37 -0600

    importamos useDispatch de react-redux para disparar las acciones

--- src/hooks/useCheckauth.js
+++ src/hooks/useCheckauth.js
@@ -1,10 +1,10 @@
 import { useEffect } from 'react'
-import { useSelector } from 'react-redux'
+import { useSelector,useDispatch } from 'react-redux'
 
 
 export const useCheckauth = () =>{
-    const { status } = useSelector(state => state.auth) //lee el status de auth sinautenticar | autenticado
+    const { status } = useSelector(state => state.auth) //lee el status de auth sinautenticar | autenticado 
     return {
-
+        status
     }
 }
