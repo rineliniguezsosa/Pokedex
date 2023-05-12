4970303d4aa759863dd1992dcfdc2de2f3e37057
rinel <rineliniguezsosa@gmail.com>
2023-05-12 13:39:50 -0600

    importando el customhook useCheckauth

--- src/router/Approuter.jsx
+++ src/router/Approuter.jsx
@@ -1,6 +1,7 @@
 import { Routes,Route,Navigate } from 'react-router-dom'
 import { Pokemonroutes } from '../pokedex/routes/Pokemonroutes'
 import { Loginroutes } from '../auth/routes/Loginroutes'
+import { useCheckauth } from '../hooks/useCheckauth'
 
 export function Approuter() {
   return (
