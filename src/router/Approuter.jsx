bb925633bdba6368ab95bbb6917f290138137ce6
rinel <rineliniguezsosa@gmail.com>
2023-05-12 13:37:08 -0600

    eliminando la importacion de useSelector de react redux

--- src/router/Approuter.jsx
+++ src/router/Approuter.jsx
@@ -1,5 +1,4 @@
 import { Routes,Route,Navigate } from 'react-router-dom'
-import { useSelector } from 'react-redux'
 import { Pokemonroutes } from '../pokedex/routes/Pokemonroutes'
 import { Loginroutes } from '../auth/routes/Loginroutes'
 
