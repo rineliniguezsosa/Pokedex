fa8c33fd62c61c2cbf938c28c5ff47769867945d
rinel <rineliniguezsosa@gmail.com>
2023-05-19 13:28:17 -0600

    agregando en la linea 55 el valor de las habilidades de los pokemones

--- src/pokedex/pages/Pokemon.jsx
+++ src/pokedex/pages/Pokemon.jsx
@@ -52,7 +52,7 @@ export const Pokemon = () => {
                         <Tablecellinh>{pokemon.name}</Tablecellinh>
                         <Tablecellinh><img alt="pokemon" src={pokemon.sprites.front_default}></img></Tablecellinh>
                         <Tablecellinh>{pokemon.types.map(tipo=>(<p key={tipo.slot}>{tipo.type.name}</p>))}</Tablecellinh>
-                        <Tablecellinh></Tablecellinh>
+                        <Tablecellinh>{pokemon.abilities.map(ability=>(<p key={ability.ability.slot}>{ability.ability.name}</p>))}</Tablecellinh>
                         <Tablecellinh></Tablecellinh>
                     </tr>
                       ))
