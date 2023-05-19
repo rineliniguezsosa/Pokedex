3ba96606b52c66b90db5dfa116ed053f65efc7f0
rinel <rineliniguezsosa@gmail.com>
2023-05-19 14:10:17 -0600

    creando el componente Shiny con las propiedades css text-decoration: none;padding: 5px 10px;font-size:10px;background-color: #47525E;border-radius: 3px; border: 2px solid #47525E; color: white;

--- src/assets/components/Enlaces.jsx
+++ src/assets/components/Enlaces.jsx
@@ -1,2 +1,12 @@
 import { Link } from 'react-router-dom'
 import styled from 'styled-components'
+
+export const Shiny = styled(Link)`
+    text-decoration: none;
+    padding: 5px 10px;
+    font-size:10px;
+    background-color: #47525E;
+    border-radius: 3px;
+    border: 2px solid #47525E;
+    color: white;
+`
