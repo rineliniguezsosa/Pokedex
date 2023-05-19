20df6f87b1ff9fc496a46f72fa463c8ff35b67f4
rinel <rineliniguezsosa@gmail.com>
2023-05-18 23:17:20 -0600

    creamos la variable req el cual esperara la respuesta de la petición hacia la api pokeapi

--- src/store/pokemon/thunks.js
+++ src/store/pokemon/thunks.js
@@ -3,6 +3,7 @@ export const startfetchingpokeapi = (page) =>{
     return async(dispatch)=>{
         try {
             const urlpokemones = []
+            const req = await fetch(`https://pokeapi.co/api/v2/pokemon/?limit=5&offset=${page}`)
         } catch (error) {
             
         }
