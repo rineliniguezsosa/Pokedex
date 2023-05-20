f8fc2513783333afad21d3a21a1c9bb13791d814
rinel <rineliniguezsosa@gmail.com>
2023-05-19 20:40:42 -0600

    cambiando el nombre del parametro pokemon por pokemonId en la ruta pokemonshiny

--- src/pokedex/routes/Pokemonroutes.jsx
+++ src/pokedex/routes/Pokemonroutes.jsx
@@ -7,7 +7,7 @@ export const Pokemonroutes = () => {
     <Routes>
         <Route path="pokemon" element={<Pokemon/>}></Route>
 
-        <Route path="pokemonshiny/:pokemon" element={<PokemonShiny/>}></Route> 
+        <Route path="pokemonshiny/:pokemonId" element={<PokemonShiny/>}></Route> 
 
         <Route path="/*" element={<Navigate to="/pokemon"/>}></Route>
     </Routes>
