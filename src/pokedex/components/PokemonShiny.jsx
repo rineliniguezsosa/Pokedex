50f55c241643e8a21a88ed8a470e2f47014b48a7
rinel <rineliniguezsosa@gmail.com>
2023-05-19 20:43:06 -0600

    desestructuramos pokemonId de useParams para obtener el id del pokemon enviado y lo imprimimos en consola para observar el comportamiento

--- src/pokedex/components/PokemonShiny.jsx
+++ src/pokedex/components/PokemonShiny.jsx
@@ -2,6 +2,8 @@ import { useParams } from "react-router-dom"
 
 
 export const PokemonShiny = () => {
+  const { pokemonId } = useParams()
+  console.log(pokemonId)
   return (
     <div>Pokemoninfo</div>
   )
