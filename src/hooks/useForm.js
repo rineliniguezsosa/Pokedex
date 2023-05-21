5c7e5f38dc82341dba2e6825f0cafd0e50faeb5f
rinel <rineliniguezsosa@gmail.com>
2023-05-20 19:48:36 -0600

    importando fetchingpokemon en useForm.js

--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,6 +1,6 @@
 import { useState } from "react"
 import { useDispatch } from "react-redux"
-import { startfetchingpokeapi } from "../store"
+import { fetchingpokemon } from "../store"
 
 export const useForm = (initialForm = {}) =>{
     const [formState, setFormState] = useState(initialForm)
