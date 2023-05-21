a049b2bcd0d61ebe29785353fba0bf837be330d7
rinel <rineliniguezsosa@gmail.com>
2023-05-20 20:42:25 -0600

    cambiando el nombre de las definiciones de formState y setFormState por pokemon y setPokemon

--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -3,30 +3,30 @@ import { useDispatch } from "react-redux"
 import { fetchingpokemon } from "../store"
 
 export const useForm = (initialForm = {}) =>{
-    const [formState, setFormState] = useState(initialForm)
+    const [pokemon, setPokemon] = useState(initialForm)
     const dispatch = useDispatch()
 
     const onInputchange = (event) =>{
         const {name,value} = event.target;
        
-        setFormState({
-            ...formState,
+        setPokemon({
+            ...pokemon,
             [name]:value
         })
     }
 
     const onSubmitform = (event) =>{
         event.preventDefault()
-
-        if(formState.pokemon.trim().length < 1){ return; }
         
-        dispatch(fetchingpokemon(formState)) //enviamos el pokemon
+        if(pokemon.pokemon.trim().length < 1){ return; }
+        
+        dispatch(fetchingpokemon(pokemon)) //enviamos el pokemon
 
         event.target.reset();
     }
     return {
-        ...formState,
-        formState,
+        ...pokemon,
+        pokemon,
         onInputchange,
         onSubmitform
     }
