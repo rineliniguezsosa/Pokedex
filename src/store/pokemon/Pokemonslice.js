3f9f7bc64e1f8eb8964d105e56dbe3004bf43402
rinel <rineliniguezsosa@gmail.com>
2023-05-20 21:14:38 -0600

    actualizando el state de pokemones por la propagacion del payload del pokemon

--- src/store/pokemon/Pokemonslice.js
+++ src/store/pokemon/Pokemonslice.js
@@ -12,7 +12,7 @@ export const pokemonSlice = createSlice({
       state.pokemones = [...payload]
     },
     savingpokemon:(state,{payload})=>{
-
+      state.pokemones = [...payload]
     }
   },
 })
