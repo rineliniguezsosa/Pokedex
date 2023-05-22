457f0507d68ef43e6bf8786cbc99b7e755f936de
rinel <rineliniguezsosa@gmail.com>
2023-05-22 16:17:55 -0600

    feat: importando useDispatch de react-redux en PokemonShiny.jsx

--- src/pokedex/pages/PokemonShiny.jsx
+++ src/pokedex/pages/PokemonShiny.jsx
@@ -1,5 +1,6 @@
 import { useParams } from "react-router-dom"
 import { Pokemoncontainer, Spritescontainer } from "../../assets"
+import { useDispatch } from "react-redux"
 
 
 export const PokemonShiny = () => {
