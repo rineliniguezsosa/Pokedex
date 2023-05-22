1fdb00bece58ba2d3e1b4b3bf6c0897724e88b03
rinel <rineliniguezsosa@gmail.com>
2023-05-22 16:23:58 -0600

    feat: importando fetchingpokemon en PokemonShiny.jsx

--- src/pokedex/pages/PokemonShiny.jsx
+++ src/pokedex/pages/PokemonShiny.jsx
@@ -2,6 +2,7 @@ import { useParams } from "react-router-dom"
 import { Pokemoncontainer, Spritescontainer } from "../../assets"
 import { useDispatch } from "react-redux"
 import { useEffect } from "react"
+import { fetchingpokemon } from "../../store"
 
 
 export const PokemonShiny = () => {
