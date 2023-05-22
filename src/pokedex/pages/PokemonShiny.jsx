9bde8c41d4a9d9190c3ff0ad7627c227b9b1c7d1
rinel <rineliniguezsosa@gmail.com>
2023-05-22 15:16:46 -0600

    feat: importando Spritescontainer en PokemonShiny.jsx
    Agregando el componente Spritescontainer en PokemonShiny.jsx

--- src/pokedex/pages/PokemonShiny.jsx
+++ src/pokedex/pages/PokemonShiny.jsx
@@ -1,11 +1,15 @@
 import { useParams } from "react-router-dom"
-import { Pokemoncontainer } from "../../assets"
+import { Pokemoncontainer, Spritescontainer } from "../../assets"
 
 
 export const PokemonShiny = () => {
   const { pokemonId } = useParams()
   console.log(pokemonId)
   return (
-    <Pokemoncontainer>Pokemoninfo</Pokemoncontainer>
+    <Pokemoncontainer>
+      <Spritescontainer>
+        
+      </Spritescontainer>
+    </Pokemoncontainer>
   )
 }
