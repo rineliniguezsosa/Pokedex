952ec5c17a52f7df7ca04e8cc0f0fcd37b80d680
rinel <rineliniguezsosa@gmail.com>
2023-05-22 16:35:09 -0600

    feat: importando useSelector  de react-redux
    useSelector nos servira para leer la informacion almacenada de pokemones

--- src/pokedex/pages/PokemonShiny.jsx
+++ src/pokedex/pages/PokemonShiny.jsx
@@ -1,6 +1,6 @@
 import { useParams } from "react-router-dom"
 import { Pokemoncontainer, Spritescontainer } from "../../assets"
-import { useDispatch } from "react-redux"
+import { useDispatch,useSelector } from "react-redux"
 import { useEffect } from "react"
 import { fetchingpokemon } from "../../store"
 
