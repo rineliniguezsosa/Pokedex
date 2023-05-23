35d3df20eb0681b39c26dd49c749cc208e86e9ec
rinel <rineliniguezsosa@gmail.com>
2023-05-22 19:49:25 -0600

    feat: pegando el contenido de tabla en PokemonList.jsx

--- src/pokedex/components/PokemonList.jsx
+++ src/pokedex/components/PokemonList.jsx
@@ -2,6 +2,30 @@
 
 export const PokemonList = () => {
   return (
-    <div>PokemonList</div>
+    <Table>
+        <thead>
+        <tr>
+            <Tablecell>#</Tablecell>
+            <Tablecell>Nombre</Tablecell>
+            <Tablecell>Vista</Tablecell>
+            <Tablecell>Tipos</Tablecell>
+            <Tablecell>Habilidades</Tablecell>
+            <th style={{maxWidth:'5%',height:'70px'}}></th>
+        </tr>
+        </thead>
+        <tbody>
+        {pokemones.map(pokemon => (
+        <tr key={pokemon.order}>
+            <Tablecellinh>{pokemon.id}</Tablecellinh>
+            <Tablecellinh>{pokemon.name}</Tablecellinh>
+            <Tablecellinh><img alt="pokemon" src={pokemon.sprites.front_default}></img></Tablecellinh>
+            <Tablecellinh>{pokemon.types.map(tipo=>(<p key={tipo.slot}>{tipo.type.name}</p>))}</Tablecellinh>
+            <Tablecellinh>{pokemon.abilities.map(ability=>(<p key={ability.ability.name}>{ability.ability.name}</p>))}</Tablecellinh>
+            <Tablecellinh><Shiny to={`/pokemonshiny/${pokemon.id}`}>Shyny</Shiny></Tablecellinh>
+        </tr>
+            ))
+        }
+        </tbody>
+    </Table>
   )
 }
