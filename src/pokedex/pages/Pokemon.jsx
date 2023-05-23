e35eb56de342be0c3f1c6c310ff5cf10a7e5895e
rinel <rineliniguezsosa@gmail.com>
2023-05-22 20:08:12 -0600

    feat: agregando el componente PokemonList en la linea 44

--- src/pokedex/pages/Pokemon.jsx
+++ src/pokedex/pages/Pokemon.jsx
@@ -41,7 +41,7 @@ export const Pokemon = () => {
             </Formbox>
 
             <Childcontainer>
-
+                <PokemonList></PokemonList>
             </Childcontainer>
             
             <Buttoncontainer>
