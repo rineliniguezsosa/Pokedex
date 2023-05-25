435c51b325d074f96c9642bd1b3ace88aadbbabe
rinel <rineliniguezsosa@gmail.com>
2023-05-25 17:39:55 -0600

    fix: eliminando y cambiando la desestructuracion de pokemon por pokemonId

--- src/store/pokemon/thunks.js
+++ src/store/pokemon/thunks.js
@@ -17,10 +17,10 @@ export const startfetchingpokemonlist = (page) =>{
     }
 }
 
-export const fetchingpokemon = ({pokemon}) =>{
+export const fetchingpokemon = (pokemonId) =>{
     return async(dispatch)=>{
         try {
-            const req = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
+            const req = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonId}`)
             const resp = await req.json()
             dispatch(savingpokemon(resp))
         } catch (error) {
