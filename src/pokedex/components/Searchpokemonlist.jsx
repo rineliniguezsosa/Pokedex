940b9b55a5fe466375c3ac8091f0ff81cf6e59f8
rinel <rineliniguezsosa@gmail.com>
2023-05-26 16:37:26 -0600

    feat: desestructurando infopokemon del state pokemon

--- src/pokedex/components/Searchpokemonlist.jsx
+++ src/pokedex/components/Searchpokemonlist.jsx
@@ -2,6 +2,7 @@ import { useSelector } from "react-redux"
 
 
 export const Searchpokemonlist = () => {
+  const { infopokemon } = useSelector(state => state.pokemon)
   return (
     <div>Searchpokemonlist</div>
   )
