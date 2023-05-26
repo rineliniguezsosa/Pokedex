77a39037e0d1aac68e263be0298a255b44888a76
rinel <rineliniguezsosa@gmail.com>
2023-05-26 16:39:01 -0600

    feat: pegando el contenido de PokemonList.jsx

--- src/pokedex/components/Searchpokemonlist.jsx
+++ src/pokedex/components/Searchpokemonlist.jsx
@@ -4,6 +4,30 @@ import { useSelector } from "react-redux"
 export const Searchpokemonlist = () => {
   const { infopokemon } = useSelector(state => state.pokemon)
   return (
-    <div>Searchpokemonlist</div>
+    <Table>
+        <thead>
+        <tr>
+            <Tablecell>#</Tablecell>
+            <Tablecell>Nombre</Tablecell>
+            <Tablecell>Vista</Tablecell>
+            <Tablecell>Tipos</Tablecell>
+            <Tablecell>Habilidades</Tablecell>
+            <th style={{maxWidth:'5%',height:'70px'}}></th>
+        </tr>
+        </thead>
+        <tbody>
+        {pokemonlist.map(pokemon => (
+        <tr key={pokemon.order}>
+            <Tablecellinh>{pokemon.id}</Tablecellinh>
+            <Tablecellinh>{pokemon.name}</Tablecellinh>
+            <Tablecellinh><img alt="pokemon" src={pokemon.sprites.front_default}></img></Tablecellinh>
+            <Tablecellinh>{pokemon.types.map(tipo=>(<p key={tipo.slot}>{tipo.type.name}</p>))}</Tablecellinh>
+            <Tablecellinh>{pokemon.abilities.map(ability=>(<p key={ability.ability.name}>{ability.ability.name}</p>))}</Tablecellinh>
+            <Tablecellinh><Shiny to={`/pokemonshiny/${pokemon.id}`}>Shyny</Shiny></Tablecellinh>
+        </tr>
+            ))
+        }
+        </tbody>
+    </Table>
   )
 }
