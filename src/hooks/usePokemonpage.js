8a00577dbad3c2f215abc8445c64cd1b546fa523
rinel <rineliniguezsosa@gmail.com>
2023-05-27 23:17:22 -0600

    feat: creando la variable dispatch el cual tendra la referencia de useDispatch

--- src/hooks/usePokemonpage.js
+++ src/hooks/usePokemonpage.js
@@ -3,7 +3,7 @@ import { useDispatch } from "react-redux"
 import { startfetchingpokemonlist } from "../store"
 
 export const usePokemonpage = (numberpage) =>{
-
+    const dispatch = useDispatch()
     return {
         
     }
