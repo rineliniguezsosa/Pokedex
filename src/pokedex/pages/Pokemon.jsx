f2fa6fc54a8830d0086bac24aae2f18f2ff628e7
rinel <rineliniguezsosa@gmail.com>
2023-05-27 23:38:28 -0600

    fix: eliminando la arrow function Siguiente

--- src/pokedex/pages/Pokemon.jsx
+++ src/pokedex/pages/Pokemon.jsx
@@ -23,10 +23,6 @@ export const Pokemon = () => {
     (page < 1) ? setPage(page) : setPage(page-5) //validaciones
   }
 
-  const Siguiente = () =>{
-    (page === 1280) ? setPage(page): setPage(page+5) //limitando la busqueda de los pokemons ya que son 1280
-  }
-  
   return (
     <Pokemoncontainer>
         <Pokedexcontainer>
